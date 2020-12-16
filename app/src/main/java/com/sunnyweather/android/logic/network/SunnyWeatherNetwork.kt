e862c46fb4205e8849e52cf85f5ea59632b35dc2
HK <1980205409@qq.com>
2020-12-16 16:30:30 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
+++ app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
@@ -11,11 +11,19 @@ import kotlin.coroutines.suspendCoroutine
 
 object SunnyWeatherNetwork {
 
+    private val weatherService = ServiceCreator.create(WeatherService::class.java)
+
+    suspend fun getDailyWeather(lng: String, lat: String) = weatherService.getDailyWeather(lng, lat).await()
+
+    suspend fun getRealtimeWeather(lng: String, lat: String) = weatherService.getRealtimeWeather(lng, lat).await()
+
     // 创建PlaceService接口的动态代理对象
     private val placeService = ServiceCreator.create(PlaceService::class.java)//.create<PlaceService>()
 
     // 调用方法发起搜索城市数据请求
     suspend fun searchPlaces(query: String) = placeService.searchPlaces(query).await()
+
+
 
     private suspend fun <T> Call<T>.await(): T {
         return suspendCoroutine { continuation ->
